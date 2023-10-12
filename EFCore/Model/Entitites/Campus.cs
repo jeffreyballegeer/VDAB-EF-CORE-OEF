195c8715c79938a68839c995eebbe3fea58786b7
Jeffrey Ballegeer <56387714+jeffreyballegeer@users.noreply.github.com>
2023-10-12 09:45:29 +0200

    Add entities Campus & Docent (4.3)

---
+++ EFCore/Model/Entitites/Campus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitites
+{
+    public class Campus
+    {
+        public int CampusId { get; set; }
+        public string Naam { get; set; }
+        public string Straat { get; set; }
+        public string Huisnummer { get; set; }
+        public string Postcode { get; set; }
+        public string Gemeente { get; set; }
+        public ICollection<Docent> Docenten { get ; set; } // navigation property (in een campus werken meerdere docenten)
+    }
+}
---
+++ EFCore/Model/Entitites/Docent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitites
+{
+    public class Docent
+    {
+        public int DocentId { get; set; }
+        public string Voornaam { get; set; }
+        public string Familienaam { get; set; }
+        public decimal Wedde { get; set; }
+        public Campus Campus { get; set; } // navigation property
+    }
+}
