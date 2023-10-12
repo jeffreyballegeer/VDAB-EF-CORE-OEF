0b0a43801af3ddfcbbf71a9f85bb8529417a553f
Jeffrey Ballegeer <56387714+jeffreyballegeer@users.noreply.github.com>
2023-10-12 11:04:54 +0200

    Add excercise 4.6 : Create new solution Taken with projects UI, Model & Tests. Add entities Rekening & Klant. Add DBContext class

---
+++ Taken/Model/Entities/EFTakenContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class EFTakenContext : DbContext
+    {
+        public DbSet<Klant> Klanten { get; set; }
+        public DbSet<Rekening> Rekeningen { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            "Server=.\\SQLExpress;Database=EFBank;" +
+            "Trusted_Connection=true;encrypt=false",    //trusted connection = windowsuser als login
+            options => options.MaxBatchSize(150));      // # sql commands die in 1* naar de db gestuurd kunnen worden
+        }
+    }
+}
---
+++ Taken/Model/Entities/Rekening.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class Rekening
+    {
+        public string RekeningNr { get; set; }
+        public int KlantNr { get; set; }
+        public decimal Saldo { get; set; }
+        public char Soort { get; set; }
+        public Klant Klant { get; set; } // nav prop
+
+        public void Storten(decimal bedrag)
+        {
+            Saldo += bedrag;
+        }
+    }
+}
