9b28d71cf09cbecbc674a9be44fb56b9cd571f14
Jeffrey Ballegeer <56387714+jeffreyballegeer@users.noreply.github.com>
2023-10-12 09:50:13 +0200

    Add DBContext class EFOpleidingenContext (4.4)

---
+++ EFCore/Model/Entitites/EFOpleidingenContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitites
+{
+    internal class EFOpleidingenContext : DbContext
+    {
+        public DbSet<Campus> Campussen { get; set; }
+        public DbSet<Docent> Docenten { get; set; }
+    }
+}
