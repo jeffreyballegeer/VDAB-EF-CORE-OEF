9218cdbada8ede32d2363ed7b1e1bab406625d29
Jeffrey Ballegeer <56387714+jeffreyballegeer@users.noreply.github.com>
2023-10-17 18:40:58 +0200

    Add excercise 6.4 : Show SQL statement to get all 'Klanten' from db

--- Taken/UI/Program.cs
+++ Taken/UI/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+
+var context = new EFTakenContext();
+foreach (Klant klant in context.Klanten)
+{
+
+}
+Console.WriteLine(context.Klanten.ToQueryString());
