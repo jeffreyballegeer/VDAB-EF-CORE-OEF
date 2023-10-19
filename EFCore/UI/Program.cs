bb392d12c6b39a6eaaaf9644b0c0536f29d73326
Jeffrey Ballegeer <56387714+jeffreyballegeer@users.noreply.github.com>
2023-10-19 18:35:57 +0200

    Refactor program.cs to use a LINQ query (6.5)

--- EFCore/UI/Program.cs
+++ EFCore/UI/Program.cs
@@ -1,8 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Model.Entitites;
 
-using var context = new EFOpleidingenContext();
-foreach (var docent in context.Docenten)
-    Console.WriteLine(docent.Naam);
-//Console.WriteLine(context.Docenten.ToQueryString());  // Log SQL query to console 
-//Console.WriteLine(context.Campussen.ToQueryString()); // Log SQL query to console 
+//Code to show all docenten from db
+//using var context = new EFOpleidingenContext();
+//foreach (var docent in context.Docenten)
+//    Console.WriteLine(docent.Naam);
+
+// Log SQL Query's to console
+//Console.WriteLine(context.Docenten.ToQueryString());
+//Console.WriteLine(context.Campussen.ToQueryString());
+
+Console.Write("Minimum wedde:");
+if (decimal.TryParse(Console.ReadLine(), out decimal minWedde))
+{
+    using var context = new EFOpleidingenContext();
+    var query = from docent in context.Docenten
+                where docent.Wedde >= minWedde
+                orderby docent.Voornaam, docent.Familienaam
+                select docent;
+    foreach (var docent in query)
+        Console.WriteLine("{0}: {1}", docent.Naam, docent.Wedde);
+}
+else
+    Console.WriteLine("Geef een getal in !");
+
