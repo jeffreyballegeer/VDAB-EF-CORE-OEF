97294b4c7e790237f986d00f40e888f49332bf90
Jeffrey Ballegeer <56387714+jeffreyballegeer@users.noreply.github.com>
2023-10-30 13:42:20 +0100

    Add excercise 8.5 : Allow deposits to accountnr

--- Taken/UI/Program.cs
+++ Taken/UI/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update;
 using Microsoft.IdentityModel.Tokens;
 using Model.Entities;
+using System.Collections.Immutable;
 using System.Net.Http.Headers;
 
-var context = new EFTakenContext();
+//var context = new EFTakenContext();
 
 // Excercise 6.13: Show all customers including any accounts with balances and total balance. Use eager loading
 
@@ -27,46 +29,78 @@ var context = new EFTakenContext();
 //            select Klant).ToList();
 //}
 
+
 // Excercise 7.6 : Voeg nieuw rekeningnr toe aan bestaande klant
 
-List<Klant> FindAllKlanten()
+//List<Klant> FindAllKlanten()
+//{
+//    using var context = new EFTakenContext();
+//    return (from Klant in context.Klanten
+//            orderby Klant.Voornaam
+//            select Klant).ToList();
+//}
+
+//foreach (var klant in FindAllKlanten())
+//{
+//    Console.WriteLine($"KlantNr {klant.KlantNr} - {klant.Voornaam}");
+//}
+
+//if (int.TryParse(Console.ReadLine(), out int klantNr))
+//{
+//    var klant = context.Klanten.Find(klantNr);
+//    if (klant != null)
+//    {
+//        Console.WriteLine("Geef een nieuw rekeningnummer in voor deze klant");
+//        string nieuwRekeningNr = Console.ReadLine() ?? string.Empty;
+//        if (!nieuwRekeningNr.IsNullOrEmpty())
+//        {
+//            Rekening rekening = new Rekening
+//            {
+//                //Klant = klant,
+//                RekeningNr = nieuwRekeningNr,
+//                KlantNr = klant.KlantNr,
+//                Saldo = Decimal.Zero,
+//                Soort = 'Z'
+//            };
+//            context.Rekeningen.Add(rekening);
+//            context.SaveChanges();
+//        }
+//        else
+//            Console.WriteLine($"Gelieve een rekeningnummer in te geven.");
+//    }
+//    else
+//        Console.WriteLine($"Klant niet gevonden");
+//}
+//else
+//    Console.WriteLine("Tik een getal");
+
+
+//Excercise 8.5 : Allow deposits to account
+
+Console.WriteLine("Geef een rekeningnr");
+string gewenstRekeningNr = Console.ReadLine();
+if (!gewenstRekeningNr.IsNullOrEmpty())
 {
     using var context = new EFTakenContext();
-    return (from Klant in context.Klanten
-            orderby Klant.Voornaam
-            select Klant).ToList();
-}
-
-foreach (var klant in FindAllKlanten())
-{
-    Console.WriteLine($"KlantNr {klant.KlantNr} - {klant.Voornaam}");
-}
-
-if (int.TryParse(Console.ReadLine(), out int klantNr))
-{
-    var klant = context.Klanten.Find(klantNr);
-    if (klant != null)
+    var gevondenRekeningNr = context.Rekeningen.Find(gewenstRekeningNr);
+    if (gevondenRekeningNr != null)
     {
-        Console.WriteLine("Geef een nieuw rekeningnummer in voor deze klant");
-        string nieuwRekeningNr = Console.ReadLine() ?? string.Empty;
-        if (!nieuwRekeningNr.IsNullOrEmpty())
+        Console.WriteLine("Welk bedrag moet gestort worden?");
+        if (decimal.TryParse(Console.ReadLine(), out var stortBedrag))
         {
-            Rekening rekening = new Rekening
+            if (stortBedrag > Decimal.Zero)
             {
-                //Klant = klant,
-                RekeningNr = nieuwRekeningNr,
-                KlantNr = klant.KlantNr,
-                Saldo = Decimal.Zero,
-                Soort = 'Z'
-            };
-            context.Rekeningen.Add(rekening);
-            context.SaveChanges();
+                gevondenRekeningNr.Storten(stortBedrag);
+                context.SaveChanges();
+                Console.WriteLine("Gestort!");
+            }
+            else
+                Console.WriteLine("Te storten bedrag moet meer zijn dan 0");
         }
         else
-            Console.WriteLine($"Gelieve een rekeningnummer in te geven.");
+            Console.WriteLine("het te storten bedrag moet een getal zijn");
     }
-    else
-        Console.WriteLine($"Klant niet gevonden");
+    else Console.WriteLine("Het ingevoerde rekeningnr bestaat niet!");
 }
 else
-    Console.WriteLine("Tik een getal");
+    Console.WriteLine("U heeft niets ingevoerd");
