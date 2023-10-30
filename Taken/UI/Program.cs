daa7dc7ed388c76029a99ca475e2bf1d5a7f431b
Jeffrey Ballegeer <56387714+jeffreyballegeer@users.noreply.github.com>
2023-10-30 11:53:34 +0100

    Add Exercise 7.6 : Add new accountnumber on existing client

--- Taken/UI/Program.cs
+++ Taken/UI/Program.cs
@@ -1,44 +1,72 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Model.Entities;
 using System.Net.Http.Headers;
 
 var context = new EFTakenContext();
 
-//Console.WriteLine(context.Klanten.ToQueryString());
+// Excercise 6.13: Show all customers including any accounts with balances and total balance. Use eager loading
 
-//foreach (var rekening in FindAllRekeningen())
+//foreach (var klant in FindAllKlantenAndRekeningen())
 //{
-//    Console.WriteLine(rekening.Klant.Voornaam);
-//    foreach (var rekening in )
-//    Console.WriteLine($"{rekening.RekeningNr}: {rekening.Saldo}");
-//    Console.WriteLine($"Totaal: {rekening.RekeningNr}");
+//    Console.WriteLine(klant.Voornaam);
+//    decimal totaal = Decimal.Zero;
+//    foreach (var rekening in klant.Rekeningen)
+//    {
+//        Console.WriteLine($"{rekening.RekeningNr}: {rekening.Saldo}");
+//        totaal += rekening.Saldo;
+//    }
+//    Console.WriteLine($"Totaal: {totaal}{Environment.NewLine}");
 //}
 
-//List<Rekening> FindAllRekeningen()
+//List<Klant> FindAllKlantenAndRekeningen()
 //{
 //    using var context = new EFTakenContext();
-//    return (from rekening in context.Rekeningen.Include("Klant")
-//            orderby rekening.Klant.Voornaam
-//            select rekening).ToList();
+//    return (from Klant in context.Klanten.Include("Rekeningen")
+//            orderby Klant.Voornaam
+//            select Klant).ToList();
 //}
 
+// Excercise 7.6 : Voeg nieuw rekeningnr toe aan bestaande klant
 
-foreach (var klant in FindAllKlantenAndRekeningen())
-{
-    Console.WriteLine(klant.Voornaam);
-    decimal totaal = Decimal.Zero;
-    foreach (var rekening in klant.Rekeningen)
-    {
-        Console.WriteLine($"{rekening.RekeningNr}: {rekening.Saldo}");
-        totaal += rekening.Saldo;
-    }
-    Console.WriteLine($"Totaal: {totaal}{Environment.NewLine}");
-}
-
-List<Klant> FindAllKlantenAndRekeningen()
+List<Klant> FindAllKlanten()
 {
     using var context = new EFTakenContext();
-    return (from Klant in context.Klanten.Include("Rekeningen")
+    return (from Klant in context.Klanten
             orderby Klant.Voornaam
             select Klant).ToList();
 }
+
+foreach (var klant in FindAllKlanten())
+{
+    Console.WriteLine($"KlantNr {klant.KlantNr} - {klant.Voornaam}");
+}
+
+if (int.TryParse(Console.ReadLine(), out int klantNr))
+{
+    var klant = context.Klanten.Find(klantNr);
+    if (klant != null)
+    {
+        Console.WriteLine("Geef een nieuw rekeningnummer in voor deze klant");
+        string nieuwRekeningNr = Console.ReadLine() ?? string.Empty;
+        if (!nieuwRekeningNr.IsNullOrEmpty())
+        {
+            Rekening rekening = new Rekening
+            {
+                //Klant = klant,
+                RekeningNr = nieuwRekeningNr,
+                KlantNr = klant.KlantNr,
+                Saldo = Decimal.Zero,
+                Soort = 'Z'
+            };
+            context.Rekeningen.Add(rekening);
+            context.SaveChanges();
+        }
+        else
+            Console.WriteLine($"Gelieve een rekeningnummer in te geven.");
+    }
+    else
+        Console.WriteLine($"Klant niet gevonden");
+}
+else
+    Console.WriteLine("Tik een getal");
